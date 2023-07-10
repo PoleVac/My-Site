a3e3b9565c5a56a1da0b1048bde9a05b7cde9832
Alejandro <alejandro.pulido.ruiz@gmail.com>
2023-07-10 12:23:50 -0400

    cat svg and fullscren

--- src/Index/VideoDisplay.tsx
+++ src/Index/VideoDisplay.tsx
@@ -14,6 +14,7 @@ function VideoDisplay({ video }: Video) {
         <iframe
           width={560 * temp}
           height={315 * temp}
+          allowFullScreen
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           src={video}
         ></iframe>

491aba06ce764f67f9dad66bf4a8afe7caec7856
Alejandro <alejandro.pulido.ruiz@gmail.com>
2023-07-10 12:37:29 -0400

    test for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,27 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import CustomHeader from "./Index/CustomHeader";
 import Button from "./Index/Button";
+import { BrowserView, MobileView } from "react-device-detect";
 
 function App() {
   return (
     <>
-      <Button />
-      <CustomHeader />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <BrowserView>
+        <Button />
+        <CustomHeader />
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>This is rendered only on browser</h1>
+      </BrowserView>
+      <MobileView>
+        <h1>This is rendered only on mobile</h1>
+      </MobileView>
     </>
   );
 }

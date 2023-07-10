59793363ebc791583462ae7acd0e98ebdda206f0
Alejandro <alejandro.pulido.ruiz@gmail.com>
2023-07-10 12:47:47 -0400

    progress to mobile added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import CustomHeader from "./Index/CustomHeader";
 import Button from "./Index/Button";
 import { BrowserView, MobileView } from "react-device-detect";
+import Profile from "./Index/Profile";
 
 function App() {
   return (
@@ -19,10 +20,10 @@ function App() {
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
-        <h1>This is rendered only on browser</h1>
       </BrowserView>
       <MobileView>
-        <h1>This is rendered only on mobile</h1>
+        <h1 className="text-light">Mobile version in Progress...</h1>
+        <Profile />
       </MobileView>
     </>
   );
---
+++ src/Index/Profile.tsx
@@ -0,0 +1,54 @@
+function Profile() {
+  const me = new Date("January 10,01");
+  let now = new Date(Date.now());
+  let age = now.getFullYear() - me.getFullYear();
+
+  return (
+    <>
+      <div
+        className="row"
+        style={{
+          backgroundImage:
+            "radial-gradient(rgba(100,30,200,0.5),rgba(85,85,100,0.5))",
+          borderRadius: 25,
+          margin: 25,
+          filter: "drop-shadow(2px 4px 6px black)",
+          padding: 25,
+        }}
+      >
+        <div className="col">
+          <p style={{ margin: 15, color: "white" }}>
+            <strong>
+              <h3>Alejandro Pulido ({age} M)</h3>
+            </strong>
+            <p>Software Developer (Mohawk College)</p>
+            <p>Solid Experience with REACT, HTML5, TSX, JS, CSS, PHP and SQL</p>
+            <p>
+              Used Unity & C# to create VR Experiences / Used Unreal Engine 5 to
+              render images and videos for multiple purposes
+            </p>
+            <p>
+              3D Modeling experience: Used Rhinoceros 3D for all my proyects,
+              now I'm learing Blender to expand my knowledge
+            </p>
+            <p>
+              Fast Learner and problem-solving oriented. All the videos, VR
+              experiences, images and this webpage was created in 6 months! I
+              have also learn on my own to assemble computers and 3D model!
+            </p>
+
+            <p>Fluent in both English and Spanish</p>
+          </p>
+        </div>
+        <div className="col">
+          <img
+            src="https://github.com/PoleVac/My-Site/blob/main/src/Index/images/Profile/profile.jpg?raw=true"
+            style={{ width: 400, height: 400, borderRadius: 400 / 2 }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
